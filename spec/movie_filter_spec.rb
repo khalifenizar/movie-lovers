6ecd79780a5cd892bf1ae0351cbfa208f7e86573
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2015-01-25 20:06:55 -0500

    Use `let` to set up filtered array
    Populate using OpenScruct instances with and without posters.

--- spec/movie_filter_spec.rb
+++ spec/movie_filter_spec.rb
@@ -1,17 +1,28 @@
+require 'ostruct'
+require 'imdb'
+
 require_relative '../lib/movie_filter'
 
 RSpec.describe 'MovieFilter' do
-    it 'returns the correct amount of movies' do
-        movies = []
+    let(:filtered) do
+        no_poster = OpenStruct.new
+        poster = OpenStruct.new
+        poster.poster = 'http://example.com'
+
+        movies = [ no_poster, no_poster ]
+        (1..20).each do
+            movies.push(poster)
+        end
+
         movie_filter = MovieFilter.new(movies)
-        filtered = movie_filter.filter(10)
+        movie_filter.filter(10)
+    end
+
+    it 'returns the correct amount of movies' do
         expect(filtered.size).to eq(10)
     end
 
     it 'removes movies without posters' do
-        movies = []
-        movie_filter = MovieFilter.new(movies)
-        filtered = movie_filter.filter(10)
         filtered.each do |movie|
             expect(movie.poster).not_to be_nil
         end
