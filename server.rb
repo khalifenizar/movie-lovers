7db567ab472d37239de542652bcded5ad703a46f
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2015-01-25 21:44:51 -0500

    Filter out movies without posters

--- server.rb
+++ server.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'imdb'
 require 'sinatra/reloader' if development?
 
+require_relative 'lib/movie_filter'
+
 get '/' do
     erb :home
 end
@@ -10,9 +12,10 @@ get '/game' do
     @title = params[:title]
 
     search = Imdb::Search.new(@title)
+    movie_filter = MovieFilter.new(search.movies)
 
     @results = search.movies.size
-    @movies = search.movies[0,9]
+    @movies = movie_filter.filter(9)
 
     erb :game
 end
