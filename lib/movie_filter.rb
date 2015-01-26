356f8fa0bd575c90d70a8e4212fa309d64e78542
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2015-01-25 20:10:54 -0500

    Implement MovieFilter#filter
    Filter out movies without posters and return only given specified amount of movies.

--- lib/movie_filter.rb
+++ lib/movie_filter.rb
@@ -1,10 +1,18 @@
-require 'ostruct'
-
 class MovieFilter
     def initialize(movies)
+        @movies = movies
     end
 
     def filter(amount)
-        return [ OpenStruct.new ]
+        filtered = []
+        checked = 0
+
+        until filtered.size == amount || checked == @movies.size
+            movie = @movies[checked]
+            filtered.push(movie) unless movie.poster.nil?
+            checked += 1
+        end
+
+        return filtered
     end
 end
